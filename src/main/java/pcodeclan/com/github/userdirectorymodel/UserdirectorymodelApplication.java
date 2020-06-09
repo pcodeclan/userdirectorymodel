1e1b71ee885594f91d5aeb286ecb78b452cf704c
User <user@Users-MacBook-Pro.local>
2020-06-09 19:03:51 +0100

    first commit

---
+++ src/main/java/pcodeclan/com/github/userdirectorymodel/UserdirectorymodelApplication.java
@@ -0,0 +1,13 @@
+package pcodeclan.com.github.userdirectorymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdirectorymodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdirectorymodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pcodeclan/com/github/userdirectorymodel/UserdirectorymodelApplicationTests.java
@@ -0,0 +1,13 @@
+package pcodeclan.com.github.userdirectorymodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdirectorymodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
