3fb579171eb240a99e284a4837aeed4eb2de6a60
User <user@Users-MacBook-Pro.local>
2020-06-10 10:34:11 +0100

    Changed test createFileFolderUSerThenSave

--- src/test/java/pcodeclan/com/github/userdirectorymodel/UserdirectorymodelApplicationTests.java
+++ src/test/java/pcodeclan/com/github/userdirectorymodel/UserdirectorymodelApplicationTests.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pcodeclan.com.github.userdirectorymodel.models.File;
 import pcodeclan.com.github.userdirectorymodel.models.Folder;
+import pcodeclan.com.github.userdirectorymodel.models.User;
 import pcodeclan.com.github.userdirectorymodel.repository.FileRepository;
 import pcodeclan.com.github.userdirectorymodel.repository.FolderRepository;
+import pcodeclan.com.github.userdirectorymodel.repository.UserRespository;
 
 @SpringBootTest
 class UserdirectorymodelApplicationTests {
@@ -17,16 +19,21 @@ class UserdirectorymodelApplicationTests {
 	@Autowired
 	FolderRepository folderRepository;
 
+	@Autowired
+	UserRespository userRespository;
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void createFileAndFolderThenSave(){
+	public void createFileFolderUserThenSave(){
 		Folder gamesFolder = new Folder("games");
 		folderRepository.save(gamesFolder);
 		File file1 = new File("Install", ".exe", 1135, gamesFolder);
 		fileRepository.save(file1);
+		User user1 = new User("Barry");
+		userRespository.save(user1);
 	}
 
 }
